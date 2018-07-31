fcdb21845394a3ab7c4c3c07d06b8a7dec549203
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-07-31 14:02:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(someValue){
-
+  if (someValue >= 42) {
+    return someValue - 42;
+  }
+  else {
+    return 42 - someValue;
+  }
 }
