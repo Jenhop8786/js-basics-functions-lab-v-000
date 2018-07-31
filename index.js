9d4e365f8ecbe5a44a7c53e0b31bde79261eaeb6
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-07-31 14:09:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,19 @@ function distanceTravelledInFeet(begin, end) {
     return (begin - end) * 264;
   }
 }
+
+function calculatesFarePrice(begin, end) {
+  const distance = distanceTravelledInFeet(begin, end);
+    if (distance <= 400) {
+      return 0;
+    }
+    else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * .02;
+    }
+    else if (distance > 2000 && distance < 2500) {
+      return 25;
+    }
+    else {
+      return "cannot travel that far";
+    }
+}
